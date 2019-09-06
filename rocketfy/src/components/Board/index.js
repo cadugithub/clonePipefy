d95052c524a3677f05c4bb71813bb10ac7cac5e6
cadugithub <carlosk2k2@gmail.com>
2019-09-06 17:25:26 -0300

    add component board

--- rocketfy/src/components/Board/index.js
+++ rocketfy/src/components/Board/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { loadLists } from '../../Services/api'
+import List from '../List'
+import { Container } from './styles';
 
-// import { Container } from './styles';
+const lists = loadLists();
 
 export default function Board() {
   return (
-    <div />
+    <Container>
+      {lists.map(list => <List key={list.title} data={list}/>)}
+    </Container>
   );
 }
